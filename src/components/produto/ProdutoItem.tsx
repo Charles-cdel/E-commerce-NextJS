e272288046a86320f54dd9428fdb8baf6ad2e301
Carlos Alberto Carneiro <lionbllack31@gmail.com>
2024-10-02 10:39:31 -0300

    iniciando a estilização

--- src/components/produto/ProdutoItem.tsx
+++ src/components/produto/ProdutoItem.tsx
@@ -9,7 +9,7 @@ export interface ProdutoItemProps{
 export default function ProdutoItem(props : ProdutoItemProps){
     const {produto} = props
 
-   return <Link href={`/produto/${produto.id}`} className=""
+   return <Link href={`/produto/${produto.id}`} className="flex flex-col bg-violet-dark border border-white/10 "
    
    >{props.produto.nome}
    </Link>
