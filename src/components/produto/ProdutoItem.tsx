8f296b60f704154598c1a621d46dc975886e6780
Carlos Alberto Carneiro <lionbllack31@gmail.com>
2024-10-02 11:23:55 -0300

    estilizando preco

--- src/components/produto/ProdutoItem.tsx
+++ src/components/produto/ProdutoItem.tsx
@@ -14,14 +14,26 @@ export default function ProdutoItem(props : ProdutoItemProps){
    rounded-xl relative max-w-[350px]" 
    
    >
-    <div>
-        <Image
+    <div className="h-48 w-full relative">
+        <Image className="object-contain"
             src={produto.imagem}
             alt="Imagem do Produto" 
             fill          
         />
     </div>
-    {props.produto.nome}
+
+    <div className="flex-1 flex flex-col p-5 gap-3 border-t border-white/10">
+        <span className="text-lg font-semibold">
+            {props.produto.nome}
+        </span>
+        <span className="text-sm border-b border-dashed self-start ">
+            {produto.especificacoes.destaque}
+        </span>
+        <div className="flex flex-col">
+            <span className="text-sm text-gray-400 line-through" >de {props.produto.precoBase}</span>
+            <span className="text-xl font-semibold text-emerald-400" >por {props.produto.precoPromocional}</span>
+        </div>
+    </div>
    </Link>
     
 }
