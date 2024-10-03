a3e6c4d1eb6cbf428c19b7e8d0c8e11e68f9b3c5
Carlos Alberto Carneiro <lionbllack31@gmail.com>
2024-10-03 20:54:02 -0300

    adicionando useEffect para executar lista de produtos

--- src/data/hooks/useProdutos.ts
+++ src/data/hooks/useProdutos.ts
@@ -2,10 +2,20 @@
 
 
 import { Produto } from "@/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function useProdutos() {
     const [produtos, setProdutos] = useState <Produto[]> ([])
+
+    async function obterProdutos(): Promise<Produto[]> {
+        const resp = await fetch ('http://localhost:4000/produtos')
+        const produtos = await resp.json()
+        return produtos ?? []
+    }
+
+    useEffect(()=>{
+        obterProdutos().then(setProdutos)
+    },[])
 
     return{
         produtos,
