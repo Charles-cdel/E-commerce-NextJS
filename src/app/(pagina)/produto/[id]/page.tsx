611d2a62052da364fb0ae2b20f12f76d302ff2cb
Carlos Alberto Carneiro <lionbllack31@gmail.com>
2024-10-03 23:29:15 -0300

    adicionando comunicacao da requisicao por id entre back e front

--- src/app/(pagina)/produto/[id]/page.tsx
+++ src/app/(pagina)/produto/[id]/page.tsx
@@ -1,12 +1,20 @@
 'use client'
 
+import { Produto } from "@/core"
 import useProdutos from "@/data/hooks/useProdutos"
+import { useEffect, useState } from "react"
 
 
 
 export default function PaginaProduto(props: any){
-   const id = +props.params.id 
-   const {produtos} = useProdutos()
-   const produto = produtos.find((p) => p.id === id)
-   return produto ? (<div>{produto?.nome}</div>) : <div>Produto não encontrado</div>
+   const {obterProdutosporId} = useProdutos ()
+   const [produto, setProduto] = useState <Produto| null>(null)
+
+   useEffect (() => {
+      obterProdutosporId(+props.params.id ).then(setProduto)
+
+   },[props.params.id])
+
+   
+   return produto ? <div>{produto?.nome}</div> : <div>Produto não encontrado</div>
 }
--- src/data/hooks/useProdutos.ts
+++ src/data/hooks/useProdutos.ts
@@ -4,11 +4,19 @@
 import { Produto } from "@/core";
 import { useEffect, useState } from "react";
 
+const urlBase = 'http://localhost:4000'
+
 export default function useProdutos() {
     const [produtos, setProdutos] = useState <Produto[]> ([])
 
     async function obterProdutos(): Promise<Produto[]> {
-        const resp = await fetch ('http://localhost:4000/produtos')
+        const resp = await fetch (`${urlBase}/produtos`)
+        const produtos = await resp.json()
+        return produtos ?? []
+    }
+
+    async function obterProdutosporId(id : number): Promise<Produto[]> {
+        const resp = await fetch (`${urlBase}/produtos/${id}`)
         const produtos = await resp.json()
         return produtos ?? []
     }
@@ -19,5 +27,6 @@ export default function useProdutos() {
 
     return{
         produtos,
+        obterProdutosporId
     }
 }
